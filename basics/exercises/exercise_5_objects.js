d1f95b149022e2c0c07fabdf1ad90f35f9d21cd4
Charl913 <charl.kruger.1472@gmail.com>
2025-11-16 21:41:28 +0200

    add exercise 5 objects

---
+++ basics/exercises/exercise_5_objects.js
@@ -0,0 +1,8 @@
+let courseData = {
+    title: 'JavaScript - The Complete Guide',
+    duration: 30,
+    levels: [
+        'Beginner',
+        'Advanced'
+    ]
+}; // Todo: Create and store an object
