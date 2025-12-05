06617a65aac980730e0b3697cc86e017c6c63589
Charl913 <charl.kruger.1472@gmail.com>
2025-12-05 15:38:19 +0200

    using if statement checking the win-condition

--- working_with_control_structures/monster_killer/assets/scripts/app.js
+++ working_with_control_structures/monster_killer/assets/scripts/app.js
@@ -1,5 +1,6 @@
 let chosenMaxLife = 100;
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
@@ -7,7 +8,18 @@ adjustHealthBars(chosenMaxLife);
 
 function attackHandler() {
     const damage = dealMonsterDamage(ATTACK_VALUE);
+    const playerDamange = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentMonsterHealth -= damage;
+    currentPlayerHealth -= playerDamange;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw');
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
